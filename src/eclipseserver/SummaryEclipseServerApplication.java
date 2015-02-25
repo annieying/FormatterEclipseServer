0ad16a574c500ffb1f84bffb48ed42d21f0ba553
annieying <annie.ying@gmail.com>
2015-02-25 12:44:33 -0500

    added command line argument flag development/deployment

--- src/eclipseserver/SummaryEclipseServerApplication.java
+++ src/eclipseserver/SummaryEclipseServerApplication.java
@@ -1,17 +1,25 @@
 package eclipseserver;
 
+import java.io.File;
+
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
 
+import formatter.CheckStyleFormatter;
+
 public class SummaryEclipseServerApplication implements IApplication {
 
-	public static final boolean TEST = true;
+	public static boolean TEST;
 	
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
+
+
+		setArguments(context);		
+		
 		System.out.println("inside SummaryEclipseServerApplication");
 		Server server = new Server(8844);
 		Context root = new Context(server,"/",Context.SESSIONS);
@@ -25,4 +33,23 @@ public class SummaryEclipseServerApplication implements IApplication {
 	public void stop() {
 	}
 
+	private void setArguments(IApplicationContext context) {
+        Object argsValue = context.getArguments().get("application.args");
+        if( argsValue == null || ! (argsValue instanceof String[]) ) {
+            System.out.println("Put either 'development' or 'deployment' as the first argument");
+        } else { 
+            String[] args = (String[])argsValue;
+            if( args.length == 0 ) {
+        		TEST = true;
+            } else {
+            	if( args[0].toLowerCase().equals("development") ) {
+            		TEST = true;
+            	} else if( args[0].toLowerCase().equals("deployment")) {
+            		TEST = false;
+            	} else {
+                    System.out.println("Require argument 'development' or 'deployment' as the first argument");            		
+            	}                
+            }
+        }
+	}
 }
