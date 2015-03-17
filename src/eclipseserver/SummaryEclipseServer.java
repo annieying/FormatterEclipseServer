289c90c99f9dae278dcc77742ff91dbfd6a5a483
annieying <annie.ying@gmail.com>
2015-03-17 11:11:38 -0400

    changed port

--- src/eclipseserver/SummaryEclipseServer.java
+++ src/eclipseserver/SummaryEclipseServer.java
@@ -8,7 +8,7 @@ import org.mortbay.jetty.servlet.ServletHolder;
 public class SummaryEclipseServer {
 		
 	public static void main(String[] args) throws Exception {				
-		Server server = new Server(8844);
+		Server server = new Server(8846);
 		Context root = new Context(server,"/",Context.SESSIONS);
 		root.addServlet(new ServletHolder(new  SummaryEclipseSerlvet()), "/*");	 
 		server.start();
